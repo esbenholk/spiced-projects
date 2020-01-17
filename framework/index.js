7716ce9c636fd5ee6910d3b8a0250c061f2b6e45
Esben Holk <esbenholknielsen@gmail.com>
2020-01-17 17:54:35 +0100

    frontpage done: includes responsive handlebar frame with moving headlines and static three js shape for intro

--- framework/index.js
+++ framework/index.js
@@ -8,34 +8,33 @@ app.use(express.static("./public"));
 const examples = require("./data.json");
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        //will look for a directory called views
-        layout: "main",
-        examples
-        // helpers: {
-        //     exclaim(text) {
-        //         return (text += "!!!!!!");
-        //     }
-        // }
-    });
+  res.render("home", {
+    layout: "main",
+    examples
+    // helpers: {
+    //     exclaim(text) {
+    //         return (text += "!!!!!!");
+    //     }
+    // }
+  });
 });
 
 app.get("/descriptions/:name", (req, res) => {
-    const currentProject = examples.find(item => item.name === req.params.name);
-    console.log(currentProject);
+  const currentProject = examples.find(item => item.name === req.params.name);
+  console.log(currentProject);
 
-    res.render("descriptions", {
-        layout: "main",
-        examples,
-        currentProject
-    });
+  res.render("descriptions", {
+    layout: "main",
+    examples,
+    currentProject
+  });
 });
 
 app.get("/houseofkilling", (req, res) => {
-    res.render("houseofkilling", {
-        layout: "main",
-        examples
-    });
+  res.render("houseofkilling", {
+    layout: "main",
+    examples
+  });
 });
 
 app.listen(8080, () => console.log("awake"));
--- framework/public/script.js
+++ framework/public/script.js
@@ -1,66 +1,26 @@
 (function() {
-    window.onload = window.onresize = function() {
-        canvas = document.getElementById("paint");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    };
-    var text = [
-        "HOUSE OF KILLING",
-        "does art",
-        "is internet",
-        "u r soft",
-        "i like u",
-        "wanna dance?",
-        "techno",
-        "ultra ponce",
-        "super",
-        "soft core",
-        "ultra ponce",
-        "super",
-        "HOUSE OF KILLING"
-    ];
-    $("body").on("mousemove", function(e) {
-        var y = e.pageY;
-        $("h1").html(text[Math.round(y / 50)]);
-    });
-
-    // $("#houseofkillinghome").addEventListener("click", function(e) {
-    //     console.log("clicked on a french girl");
-    // });
-
-    var canvas = document.getElementById("paint");
-    var ctx = canvas.getContext("2d");
-
-    document.addEventListener("mousemove", function(e) {
-        var gradient = ctx.createRadialGradient(
-            e.pageX,
-            e.pageY,
-            30,
-            e.pageX,
-            e.pageY,
-            400
-        );
-        if (window.location.pathname == "/houseofkilling") {
-            gradient.addColorStop(0, `rgba(0,0,255,0.1)`);
-            gradient.addColorStop(0.5, `rgba(0,200,100,0.1)`);
-            gradient.addColorStop(1, `rgba(20,0,20, 1)`);
-        } else {
-            gradient.addColorStop(0, `rgba(255,0,20,0.1)`);
-            gradient.addColorStop(0.5, `rgba(255,0,200,0.1)`);
-            gradient.addColorStop(1, `rgba(20,0,20, 1)`);
-        }
-
-        let x = e.pageX;
-        let y = e.pageY;
-        // console.log(x, y);
-        // console.log(radius);
-
-        ctx.beginPath();
-        ctx.arc(x, y, 200, 0, Math.PI * 2); //5 arguments: x,y,radius, startangle, accumulated angle
-        ctx.filter = "blur(10px)";
-        ctx.fillStyle = gradient;
-        ctx.strokeStyle = "rgba(0,0,0,0)";
-        ctx.fill();
-        ctx.stroke();
-    });
+  window.onload = window.onresize = function() {
+    canvas = document.getElementById("paint");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  };
+  var text = [
+    "HOUSE OF KILLING",
+    "does art",
+    "welcome to the internet",
+    "u r soft",
+    "i like u",
+    "PSYCEDELIC SPIRITUAL POP ART",
+    "R U LOST?",
+    "ULTRA SOFT",
+    "techno",
+    "soft core",
+    "ultra ponce",
+    "JENNIFER ANISTON SUPERFANS",
+    "HOUSE OF KILLING"
+  ];
+  $("body").on("mousemove", function(e) {
+    var y = e.pageY;
+    $("h1").html(text[Math.round(y / 50)]);
+  });
 })();
