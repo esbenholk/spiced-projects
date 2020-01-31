d3b9e2b46f9d88899d1a6a432a6c7063763ba248
Esben Holk <esbenholknielsen@gmail.com>
2020-01-31 10:38:37 +0100

    wrote variable PORT for process.env in index.js for deployment

--- index.js
+++ index.js
@@ -6,36 +6,37 @@ app.set("view engine", "handlebars"); //handlebar is templating language
 app.use(express.static("./projects"));
 app.use(express.static("./public"));
 const examples = require("./data.json");
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        //will look for a directory called views
-        layout: "main",
-        examples
-        // helpers: {
-        //     exclaim(text) {
-        //         return (text += "!!!!!!");
-        //     }
-        // }
-    });
+  res.render("home", {
+    //will look for a directory called views
+    layout: "main",
+    examples
+    // helpers: {
+    //     exclaim(text) {
+    //         return (text += "!!!!!!");
+    //     }
+    // }
+  });
 });
 
 app.get("/descriptions/:name", (req, res) => {
-    const currentProject = examples.find(item => item.name === req.params.name);
-    console.log(currentProject);
+  const currentProject = examples.find(item => item.name === req.params.name);
+  console.log(currentProject);
 
-    res.render("descriptions", {
-        layout: "main",
-        examples,
-        currentProject
-    });
+  res.render("descriptions", {
+    layout: "main",
+    examples,
+    currentProject
+  });
 });
 
 app.get("/houseofkilling", (req, res) => {
-    res.render("houseofkilling", {
-        layout: "main",
-        examples
-    });
+  res.render("houseofkilling", {
+    layout: "main",
+    examples
+  });
 });
 
-app.listen(8080, () => console.log("awake"));
+app.listen(PORT, () => console.log("awake"));
